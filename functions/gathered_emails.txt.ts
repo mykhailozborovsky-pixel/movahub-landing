1c44d17dd1542bb27fec314dc3961cd5f4d22aed
mykhailozborovsky-pixel <mykhailozborovsky@gmail.com>
2025-10-27 00:30:36 +0200

    Create gathered_emails.txt.ts

---
+++ functions/gathered_emails.txt.ts
@@ -0,0 +1,5 @@
+export const onRequestGet: PagesFunction<{ EMAILS: R2Bucket }> = async ({ env }) => {
+  const obj = await env.EMAILS.get('gathered_emails.txt');
+  const body = obj ? await obj.text() : '';
+  return new Response(body, { headers: { 'content-type': 'text/plain; charset=utf-8' } });
+};
